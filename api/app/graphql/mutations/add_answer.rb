a56dc22ff812d65c942e4f553e2bfcb39b52dcc1
Ryoga Saito <contact@proelbtn.com>
2025-01-17 02:10:24 +0900

    問題環境がない回答が作成されてしまうのを修正した

--- api/app/graphql/mutations/add_answer.rb
+++ api/app/graphql/mutations/add_answer.rb
@@ -14,16 +14,16 @@ module Mutations
       args = { problem: problem }
       Acl.permit!(mutation: self, args: args)
 
+      # 最初に問題環境があるかをチェックする。もしなければ回答は作成させない。
+      # ただし、現地問題についてはその限りではないので、問題コードがlocal_problem_codesに含まれている場合はチェックしない
+      if Config.local_problem_codes.split(",").exclude?(problem.code)
+        pes = ProblemEnvironment.lock.where(problem_id: problem_id, status: "UNDER_CHALLENGE", team: self.current_team!)
+        raise RecordNotExists.new(ProblemEnvironment, problem_id: problem_id, status: "UNDER_CHALLENGE", team_id: self.current_team!.id) if pes.empty?
+        pes.each { |pe| pe.update!(status: "UNDER_SCORING") }
+      end
+
       answer = Answer.new
-
       if answer.update(args.merge(bodies: bodies, confirming: false, team: self.current_team!))
-        # for local problem
-        if Config.local_problem_codes.split(",").exclude?(problem.code)
-          pes = ProblemEnvironment.lock.where(problem_id: problem_id, status: "UNDER_CHALLENGE", team: self.current_team!)
-          raise RecordNotExists.new(ProblemEnvironment, problem_id: problem_id, status: "UNDER_CHALLENGE", team_id: self.current_team!.id) if pes.empty?
-          pes.each { |pe| pe.update!(status: "UNDER_SCORING") }
-        end
-
         # TODO: answer.gradeをジョブで実行する -> after create hook
         answer.grade(percent: nil)
         Notification.notify(mutation: self.graphql_name, record: answer)
